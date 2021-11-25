e2e02f5d6747feb45aadec53f3d5ec3342eec7c5
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-11-25 11:57:16 +0200

    Fixed typos

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const promisePool = pool.promise();
 const getUser = async (userId, next) => {
   try {
     const [row] = await promisePool.query("SELECT * FROM users WHERE user_id = ?", [userId]);
-    delete row[0].password;
+    delete row[0].passwd;
     return row[0];
   } catch (e) {
     console.error('error', e.message);
