0994c4dc50487ba6fb11a5529024a48f2905efb8
Jani Salo <janikristian.salo@gmail.com>
2021-12-07 11:18:42 +0200

    getReviews returns average score for user

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,11 +3,17 @@
 const reviewModel = require("../models/reviewModel");
 const {httpError} = require("../utils/errors");
 
-// TODO calculate and send average only
+//Returns average score for user
 const getReviews = async (req, res, next) => {
   const reviews = await reviewModel.getReviews(req.params.id, next);
-  if (reviews) {
-    res.json(reviews);
+  let average = 0
+  for (let i = 0; i < reviews.length; i++){
+    average += reviews[i].score;
+  }
+  average = average / reviews.length;
+  let rounded = Math.round(average * 10) / 10;
+  if (rounded >= 0) {
+    res.json(rounded);
     return;
   }
   const err = httpError("reviews not found", 404);
