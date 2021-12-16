cc704dbf7248079188fe965fdcd65e579c61f1c9
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-12-16 12:10:24 +0200

    Added README

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Backend
+# For-Sale - Online marketplace backend
 
+---
+
+A node.js backend with MariaDB database for an online marketplace.
+
+##What's included?
+
+This package contains almost everything one needs to get their online marketplace started. Included is the entire node.js backend,
+alongside the .sql file you need to execute to get the database sorted out accordingly.
+
+## How to use
+You need to have [node.js installed](https://nodejs.org/en/download/) on your local machine or server, after which you can either download the latest
+release of this repo or simply do a ``git pull`` on it. Remember to run ``npm install`` afterwards to install 
+all the dependencies. 
+
+After this, you need to [set up a MariaDB database](https://mariadb.com/get-started-with-mariadb/),
+after which you can simply run ``mysql db_name < database_queries.sql`` to get the database sorted out.
+
+In your backend folder, create a .env file according to following:
+```
+DB_HOST=<ip-address-of-server>
+DB_USER=<mariadb-username>
+DB_PASS=<mariadb-password>
+DB_NAME=<mariadb-database-name>
+JWT_SECRET=<secret-of-your-choice>
+```
+Fill the file according to your MariaDB setup. Remember to use ``http://localhost:3000`` as ip if you're running
+on local machine. Otherwise, use the server address.
+
+That's it, you're done! You can check out an example frontend utilizing this backend [<ins>here</ins>](http://10.114.34.81/~forsale/frontend/).
+
+## Authors
+
+_Markus Nivasalo_
+- Email: _markus.nivasalo (at) metropolia.fi_
+- Git: https://github.com/markusniv
+
+_Jani Salo_
+- Email: _jani.salo2 (at) metropolia.fi_
+- Git: https://github.com/janiksa
+
+_Oskari Arponen_
+- Email: _oskari.arponen (at) metropolia.fi_
+- Git: https://github.com/AOskari
--- database_queries.sql
+++ database_queries.sql
@@ -1,46 +1,146 @@
-CREATE TABLE users (
-  user_id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  passwd VARCHAR(100) NOT NULL,
-  role INT NOT NULL DEFAULT 1,
-  profile_pic VARCHAR(100),
-  joined_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY(user_id)
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 16.12.2021 klo 10:05
+-- Palvelimen versio: 5.5.68-MariaDB
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `forsale`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Rakenne taululle `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) NOT NULL,
+  `listing_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM AUTO_INCREMENT=129 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Rakenne taululle `listings`
+--
+
+CREATE TABLE IF NOT EXISTS `listings` (
+  `listing_id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `filename` varchar(100) NOT NULL,
+  `description` text,
+  `price` int(11) NOT NULL,
+  `listing_date` datetime NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL DEFAULT 'Helsinki'
+) ENGINE=MyISAM AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Rakenne taululle `reviews`
+--
+
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `user_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `reviewer_id` int(11) NOT NULL,
+  `unique_key` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE listings (
-  listing_id INT NOT NULL AUTO_INCREMENT,
-  seller_id INT NOT NULL,
-  filename VARCHAR(100) NOT NULL,
-  description TEXT,
-  price INT NOT NULL,
-  title VARCHAR(50) NOT NULL,
-  listing_date DATETIME NOT NULL,
-  PRIMARY KEY (listing_id),
-  FOREIGN KEY (seller_id) REFERENCES users(user_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
-CREATE TABLE reviews (
-  user_id INT NOT NULL,
-  score INT NOT NULL,
-  reviewer_id INT NOT NULL,
-  unique_key INT NOT NULL,
-  PRIMARY KEY (unique_key),
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (reviewer_id) REFERENCES users(user_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- Rakenne taululle `users`
+--
 
-CREATE TABLE comments (
-  comment_id INT NOT NULL AUTO_INCREMENT,
-  listing_id INT NOT NULL,
-  user_id INT NOT NULL,
-  comment TEXT NOT NULL,
-  comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (comment_id),
-  FOREIGN KEY (listing_id) REFERENCES listings(listing_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `passwd` varchar(100) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '1',
+  `profile_pic` varchar(100) DEFAULT '0',
+  `joined_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM AUTO_INCREMENT=98 DEFAULT CHARSET=utf8;
 
-CREATE UNIQUE INDEX index_reviews_unique_key ON reviews(unique_key);
+--
+-- Vedos taulusta `users`
+--
+
+INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `passwd`, `role`, `profile_pic`, `joined_date`) VALUES
+(94, 'admin', 'admin', 'admin@admin.fi', '$2a$10$6mt8DQtMzjzqz9xY.SZvk.G7ghiFxgoEXR0ef6lNJ2JTan4CsE6CG', 0, '0', '2021-12-15 10:23:56');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `listing_id` (`listing_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `listings`
+--
+ALTER TABLE `listings`
+  ADD PRIMARY KEY (`listing_id`),
+  ADD KEY `seller_id` (`seller_id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`unique_key`),
+  ADD UNIQUE KEY `index_reviews_unique_key` (`unique_key`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `reviewer_id` (`reviewer_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=129;
+--
+-- AUTO_INCREMENT for table `listings`
+--
+ALTER TABLE `listings`
+  MODIFY `listing_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=83;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=98;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
